c1150984123fc2f40529437ae9796f5dede8a46c
w326004741 <G00329371@gmit.ie>
2018-02-11 13:52:11 +0000

    update solution 2.collatz-list

--- 2.collatz-list.rkt
+++ 2.collatz-list.rkt
@@ -7,21 +7,30 @@
 ; So collatz-list should return a list whose first element is n0
 ; the second element is n1, and so on
 
-; ni+1 = 3ni + 1 (if ni is odd), otherwise ni+1 = ni / 2
 ; > (collatz-list 5) ===>  '(5 16 8 4 2 1)
 ; > (collatz-list 9) ===> '(9 28 14 7 22 11 34 17 52 26 13 40 20 10 5 16 8 4 2 1)
 ; > (collatz-list 2) ===> '(2 1)
 
 ; cond reference: https://docs.racket-lang.org/guide/conditionals.html
-; Each test-expr is evaluated in order. If it produces #f, the corresponding bodys are ignored, and evaluation proceeds to the next test-expr. As soon as a test-expr produces a true value, its bodys are evaluated to produce the result for the cond form, and no further test-exprs are evaluated.
+; cond :Each test- expr is evaluated in order. If it produces #f, the corresponding bodys are ignored, and evaluation proceeds to the next test-expr. As soon as a test-expr produces a true value, its bodys are evaluated to produce the result for the cond form, and no further test-exprs are evaluated.
+
+; ni+1 = 3ni + 1 (if ni is odd), otherwise ni+1 = ni / 2
+; (define (collatz-list n)
+;   (cond
+;     ((= n 1) '(1)) ;true->end
+;     ((odd? n) ; if n is odd, then -> ni+1 = 3ni + 1 
+;      (cons n (collatz-list (+ (* 3 n) 1))))
+;     (else     ; otherwise -> ni+1 = ni /2
+;      (cons n (collatz-list (/ n 2))))))
+
 (define (collatz-list n)
   (cond
     ((= n 1) '(1)) ;true->end
-    ((odd? n) ; if n is odd, then -> ni+1 = 3ni + 1 
+    ((= (modulo n 2) 1) ; if n is odd, then -> n modulo 2 always = 1, so -> ni+1 = 3ni + 1
      (cons n (collatz-list (+ (* 3 n) 1))))
-    (else     ; otherwise -> ni+1 = ni /2
+    ((= (modulo n 2) 0)     ; if n is even , then -> n modulo 2 alway = 0, so -> ni+1 = ni /2
      (cons n (collatz-list (/ n 2))))))
-
+     
 (collatz-list 5)
 (collatz-list 9)
 (collatz-list 2)
